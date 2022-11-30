bd54dc6440c33aff4d7b61b037ef4474c0ac99f1
Mike Cowell <spike@spikeandbetty.com>
2022-11-29 19:06:36 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ramsey_Stair_CRUD_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
